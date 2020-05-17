0e60b2618e31f6bcd0f7348f9006aacd38a10a47
Eduardo Cavalcante <eduardo.cavalcantess@gmail.com>
2020-05-17 19:06:24 -0300

    Adicionado for + mensagem Fim

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Ok");
+		for(int i = 0; i < 10; i++) {
+			System.out.print("---Inicio---");
+			System.out.print("---Fim---");
+		}
 
 	}
 
